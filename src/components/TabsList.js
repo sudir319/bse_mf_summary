268890bc119e33735cc3a7d3e6b4ceab22143ee6
unknown <sudir.319@gmail.com>
2022-04-08 16:08:17 +0530

    Added BSE Stocks

--- src/components/TabsList.js
+++ src/components/TabsList.js
@@ -1,17 +1,22 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import BSEDayTabs from './bse/BSEDayTabs';
+import BSEStocks from './bse_stocks/BSEStocks';
 import MFTabs from './sbimf/MFTabs';
 
 const TabsList = () => (
   <Tabs>
     <TabList>
       <Tab><nobr>BSE Indices</nobr></Tab>
+      <Tab><nobr>BSE Stocks</nobr></Tab>
       <Tab><nobr>Mutual Fund Stats</nobr></Tab>
     </TabList>
 
     <TabPanel>
         <BSEDayTabs/>
+    </TabPanel>
+    <TabPanel>
+        <BSEStocks/>
     </TabPanel>
     <TabPanel>
         <MFTabs/>
---
+++ src/components/bse_stocks/IndexDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+const IndexDetails = ({indexData}) => {
+    const {scripname, prevdayclose, openrate, ltradert} = indexData;
+    const {lowrate, highrate, change_val, change_percent} = indexData;
+    const latest_val = parseInt(ltradert);
+    const chg_percent = parseFloat(change_percent);
+    const color = change_val > 0 ? "green" : "red";
+
+    return (<tr>
+        <td align="left"><nobr>{scripname}</nobr></td>
+        <td><nobr>{prevdayclose}</nobr></td>
+        <td><nobr>{openrate}</nobr></td>
+        <td><nobr>{latest_val}</nobr></td>
+        <td><nobr>{lowrate}</nobr></td>
+        <td><nobr>{highrate}</nobr></td>
+        <td><font color={color}>{change_val}</font></td>
+        <td><font color={color}>{chg_percent}</font></td>
+    </tr>);
+}
+ 
+export default IndexDetails;
